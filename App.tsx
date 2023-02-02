c9a0b98e12249925f93dd7d69dbb0c626048f110
SirLucasLima <doislcompany@gmail.com>
2023-02-02 13:45:22 -0300

    done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import './src/lib/dayjs';
+
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
+import * as Notifications from 'expo-notifications';
+
+import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
+import { useEffect } from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  async function schedulePushNotification() {
+    const schedule = await Notifications.getAllScheduledNotificationsAsync();
+    console.log("Agendadas: ", schedule);
+
+    if (schedule.length > 0) {
+      await Notifications.cancelAllScheduledNotificationsAsync();
+    }
+
+    const trigger = new Date(Date.now());
+    trigger.setHours(trigger.getHours() + 5);
+    trigger.setSeconds(0);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Olá, Rodrigo! 😀",
+        body: "Você praticou seus hábitos hoje?"
+      },
+      trigger
+    });
+  }
+
+  useEffect(() => {
+    schedulePushNotification();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
